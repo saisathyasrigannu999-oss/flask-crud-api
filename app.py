afd641e2e66fa995f041c7041c3ecd9163b95390
Sai Sathya Sri Gannu <saisathyasrigannu999@gmail.com>
2026-09-08 21:18:29 +0530

    Initial commit - Flask CRUD API

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@db:5432/flaskdb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Flask CRUD API is running"})
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    data = request.get_json()
+
+    user = User(
+        name=data["name"],
+        email=data["email"]
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        "message": "User created successfully",
+        "id": user.id
+    }), 201
+
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = User.query.all()
+
+    return jsonify([
+        {
+            "id": user.id,
+            "name": user.name,
+            "email": user.email
+        }
+        for user in users
+    ])
+
+
+@app.route("/users/<int:id>", methods=["GET"])
+def get_user(id):
+    user = User.query.get_or_404(id)
+
+    return jsonify({
+        "id": user.id,
+        "name": user.name,
+        "email": user.email
+    })
+
+
+@app.route("/users/<int:id>", methods=["PUT"])
+def update_user(id):
+    user = User.query.get_or_404(id)
+    data = request.get_json()
+
+    user.name = data.get("name", user.name)
+    user.email = data.get("email", user.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "User updated successfully"})
+
+
+@app.route("/users/<int:id>", methods=["DELETE"])
+def delete_user(id):
+    user = User.query.get_or_404(id)
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({"message": "User deleted successfully"})
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(host="0.0.0.0", port=5000, debug=True)
